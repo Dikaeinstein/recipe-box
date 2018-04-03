d42f01d1010cfc25fe85ec3304b7d5c79eccd4ee
Dikaeinstein <Dikaeinstein@users.noreply.github.com>
2018-04-03 15:52:35 +0100

    Refactored Title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Title = (props) => {
-
-  return (
-    <header
-      className="container-fluid bg-dark text-center text-light pt-3 pb-3"
-    >
-      <h1>{props.title}</h1>
-    </header>
-  );
-};
+const Title = props => (
+  <header
+    className="container-fluid bg-dark text-center text-light pt-3 pb-3"
+  >
+    <h1>{props.title}</h1>
+  </header>
+);
 
 Title.propTypes = {
   title: PropTypes.string.isRequired,
